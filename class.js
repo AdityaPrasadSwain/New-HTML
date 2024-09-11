fa25e0530c3f79ef69ac630142dac6644be69448
Adityaps52 <swainaditya921@gmail.com>
2024-09-11 10:48:04 +0530

    js

---
+++ class.js
@@ -0,0 +1,74 @@
+// let num1 = 7;
+// let num2 = 9;
+// let rut = '*';
+
+
+// switch (rut) {
+//     case "+":
+//         console.log("output = ",num1+num2);
+//         break;
+//     case "-":
+//         console.log("output = ",num1-num2);
+//         break;
+//     case "*":
+//         console.log("output = ",num1*num2);
+//         break;
+//     case "/":
+//         console.log("output = ",num1/num2);
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// let Mark1 = 50
+// let Mark2 = 50
+// let Mark3 = 50
+// let Mark4 = 90
+// let Mark5 = 80
+// let Mark6 = 60
+
+// let total = Mark1+Mark2+Mark3+Mark4+Mark5+Mark6
+// console.log("total matrk = ",total);
+// let per = (total/600)*100;
+// console.log("total per = ",per);
+// switch (per) {
+//     case per>90 && per<100:
+//         console.log("A");
+//         break;
+//         case per>80 && per<90:
+//             console.log("B");
+//         break;
+//         case per>70 && per<80:
+//             console.log("C");
+//         break;
+//         case per>60 && per<70:
+//             console.log("D");
+//         break;
+//         case 60:
+//             console.log("E");
+//         break;
+//     default:
+//         console.log("fail");
+//         break;
+// }
+
+
+let h = 160;
+let w = 65;
+
+if(h >= 150 && w >= 80){
+    console.log("fat");
+}
+else if(h <= 150 && h >= 130 && w <= 80 && w >= 65){
+    console.log("fit");
+}
+else{
+    console.log("thin");
+}
+
+
+
+
+
